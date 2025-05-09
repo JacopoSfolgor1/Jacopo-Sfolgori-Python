dab0316558daa85c192c5e3fbc516e8c37ab337e
JacopoSfolgor1 <j.sfolgori@its-ictacademy.com>
2025-05-09 14:33:06 +0200

    asfsa

--- Lezione_15/main.py
+++ Lezione_15/main.py
@@ -1,9 +1,14 @@
-PATH: str = "Lezione_15/test.txt"
-mode: str = "r"
+'''PATH: str = "Lezione_15/test.txt"
+mode: str = "w"
 econding: str = "utf-8"
 
-file = open(PATH)
-
-output: str = file.read()
+file = open(PATH, mode)
+message: str = "FPUZZ"
+output: str = file.write(message)
 
 print(output)
+file.close()'''
+
+with open("Lezione_15/test.txt","r") as file:
+    print(file.read())
+
